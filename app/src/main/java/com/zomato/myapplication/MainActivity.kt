6692a504897ea0524adfbfb80bbdad4af9a2ab0a
Arnav Gupta <arnav.gupta@zomato.com>
2019-06-11 11:45:41 +0530

    basic application
    Signed-off-by: Arnav Gupta <arnav.gupta@zomato.com>

---
+++ app/src/main/java/com/zomato/myapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.zomato.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button3.setOnClickListener {
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+
+            val a = editText.text.toString().toInt()
+            val b = editText2.text.toString().toInt()
+
+            val c = (a + b).toString()
+
+            textView.text = c
+        }
+    }
+}
